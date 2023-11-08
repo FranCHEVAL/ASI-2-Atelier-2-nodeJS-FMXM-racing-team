427d7cdd6c3344fd2b8868db8b1bc2b054c31dfe
FranCHEVAL <francois.cheval@cpe.fr>
2023-11-08 14:37:57 +0100

    broadcast and selected message base

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('join',(room) => {
+    socket.join(room);
+  })
+
+  socket.on('message', (data) => {
+    io.to(data.room).emit('message', data.message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
